8f6126f4828d895e96178f785e86bcbb0787da68
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-05-03 19:53:35 +0100

    The Mega Adventure

---
+++ precourse_recap.py
@@ -0,0 +1,56 @@
+
+print()
+print("Well Howdy stranger, I don't recognise you.")
+print()
+name = input("What is your name? ")
+print()
+print(name + "? that sounds almost Persian, I bet you work out..anyway..I need someone to join me on the shortest of adventures.")
+print()
+adventure_time = input ("Will you be that person? (Yes/No) ")
+print()
+if adventure_time == "No":
+  print("*.....Skipping to the end *")
+  print()
+  print("Wow what an adventure eh " + name + "? can't believe you nearly thought about skipping this. The Lizard King is dead and all the golden coins are back in the Museum")
+  
+  exit("GAME OVER")
+else:
+  print("Awesome, lets do this " + name)
+print()
+print("* You enter the first room and are faced with a mighty beast who demands you choose from 3 passwords, only 1 will grant you passage to the treasure *")
+print()
+print("The Lizard King - CHOOSE OR DIE!")
+print()
+passwords = "Tiddly - Widdly - Spiddly"
+print(passwords)
+print()
+choice = input("WHAT IS YOUR ANSWER! ") 
+if choice == "Widdly":
+  print()
+  print("* INCORRECT *" "and The Lizard King bites your head off....Soz")
+  exit("GAME OVER")
+if choice == "Tiddly":
+  print()
+  print("* INCORRECT! *" " and The Lizard King makes fun of your shoes")
+  print()
+  print("The Lizard King - " "Did your Mother buy you those for school?")
+  print()
+  print("* Then he bites your head off.....thats gotta suck *")
+  exit("GAME OVER")
+if choice == "Spiddly":
+  print()
+  print("NOOOOO!......Spiddly my one weakness....how did you know!")
+
+print()
+print("* The Lizard King turns into marshmallow and melts under the pressure of his shame *")
+print()
+print("Now lets get that treasure and please watch your step, otherwise youll be scrubbing lizard-mallow off your shoes forever")
+print()
+print ("* You walk up to a fancy red and black treasure chest *")
+print()
+Ttime = input ("Do you want to open it? (Yes/No) ")
+print()
+if Ttime == "No":
+  print("Now you'r just being awkward... pssst the answer is Yes")
+else:
+  print("https://www.youtube.com/watch?v=Er_9gSKWpYU")
